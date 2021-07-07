b02f88459810c451c5ec0e2c87890817129ae0ed
ulyavrubel <uvrubel@gmail.com>
2021-07-07 15:10:13 +0300

    1 file modified

--- arrays-101/findDisappearedNumbers.js
+++ arrays-101/findDisappearedNumbers.js
@@ -13,19 +13,61 @@
 //     return res;
 // };
 
+// var findDisappearedNumbers = function(nums) {
+//   const res = [];
+//   const hash = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     hash[nums[i]] = 1;
+//   }
+
+//   for (let i = 1; i <= nums.length; i++) {
+//     if (!hash[i]) res.push(i);
+//   }
+
+//   return res;
+// };
+
+// var findDisappearedNumbers = function(nums) {
+//   const res = Array(nums.length).fill(null);
+
+//   for (let i = 0; i < nums.length; i++) {
+//     res[nums[i] - 1] = nums[i]
+//   }
+
+//   for (let i = 0; i < res.length; i++) {
+//     if (res[i] === null) {
+//       res[i] = i + 1;
+//     } else {
+//       res[i] = null;
+//     }
+//   }
+
+//   for (let i = 0; i < res.length; i++) {
+//     if (res[i] === null) {
+//       res.splice(i, 1);
+//       i--;
+//     }
+//   }
+
+//   return res;
+// };
+
 var findDisappearedNumbers = function(nums) {
-  const res = [];
-  const hash = {};
+  const res = Array(nums.length).fill(null);
 
   for (let i = 0; i < nums.length; i++) {
-    hash[nums[i]] = 1;
+    res[nums[i] - 1] = nums[i];
   }
 
-  for (let i = 1; i <= nums.length; i++) {
-    if (!hash[i]) res.push(i);
-  }
+  const finalRes = [];
 
-  return res;
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === null) {
+      finalRes.push(i + 1);
+    }
+  }
+  return finalRes;
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
