0c8bf71df14f19266a7e3f0fc2751fc11ca1883f
ulyavrubel <uvrubel@gmail.com>
2021-08-20 10:48:58 +0300

    1 file modified

--- arrayAndString/arrayPairSum.js
+++ arrayAndString/arrayPairSum.js
@@ -1,7 +1,7 @@
 // Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 
 var arrayPairSum = function(nums) {
-  nums.sort();
+  nums.sort((a, b) => a - b);
 
   let sum = 0;
 
